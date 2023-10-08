2334446b9d2610d39ad5b642d69cac271e18114d
Rishi Pratap <rp0258@srmist.edu.in>
2023-10-08 13:34:23 +0530

    add fix

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const webrtc = require("wrtc");
+
 
 let senderStream;
 
 app.use(express.static('public'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
